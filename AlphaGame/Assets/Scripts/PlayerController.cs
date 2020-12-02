1065a9711de4ccd631542a6b42a902a16321c062
B00123338 <71905609+B00123338@users.noreply.github.com>
2020-12-02 23:59:18 +0000

    Alpha version of my game.

---
+++ AlphaGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+
+    public float speed = 20.0f;
+
+    public float xRange = 10.0f;
+    public float zBoundary = 10.0f;
+
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        MovePlayer();
+        ConstrainPlayerPosition();
+
+    }
+
+    void MovePlayer()
+    {
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Fire the weapon
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
+    }
+
+    void ConstrainPlayerPosition()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+
+        if (transform.position.z < -zBoundary)
+        {
+            transform.position = new Vector3(-zBoundary, transform.position.x, transform.position.y);
+        }
+        if (transform.position.z > zBoundary)
+        {
+            transform.position = new Vector3(zBoundary, transform.position.x, transform.position.y);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy2"))
+            {
+                Debug.Log("Player collided with an object");
+            }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ AlphaGame/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public GameObject powerup;
+
+    private float enemySpawnZ = 14.0f;
+    private float spawnRangeX = 18.0f;
+    private float zPowerupRange = 6.0f;
+    private float spawnY = 0.65f;
+
+    private float powerupSpawnTime = 4.0f;
+    private float enemySpawnTime = 2.5f;
+    private float startDelay = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomEnemy", startDelay, enemySpawnTime);
+        InvokeRepeating("SpawnPowerup", startDelay, powerupSpawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnRandomEnemy();
+        SpawnPowerup();
+    }
+
+    void SpawnRandomEnemy()
+    {
+        float randomX = Random.Range(-spawnRangeX, spawnRangeX);
+
+        int randomIndex = Random.Range(0, enemies.Length);
+        Vector3 spawnPos = new Vector3(randomX, spawnY, enemySpawnZ);
+        
+        Instantiate(enemies[randomIndex], spawnPos, enemies[randomIndex].gameObject.transform.rotation);
+    }
+
+    void SpawnPowerup()
+    {
+        float randomX = Random.Range(-spawnRangeX, spawnRangeX);
+        float randomZ = Random.Range(-zPowerupRange, zPowerupRange);
+
+        Vector3 spawnPos = new Vector3(randomX, spawnY, randomZ);
+        Instantiate(powerup, spawnPos, powerup.gameObject.transform.rotation);
+    }
+
+ }
