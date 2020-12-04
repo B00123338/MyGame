7b4592fda7feac4afc31e1bdfec7a434da158aee
B00123338 <71905609+B00123338@users.noreply.github.com>
2020-12-04 12:53:47 +0000

    UI added.

---
+++ Button/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class ChangeScene : MonoBehaviour
+{
+    public string URL = "https://github.com/B00123338/MyGame";
+   public void GoToScene1()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+
+    public void GoToScene2()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+
+    public void loadurl()
+    {
+        Application.OpenURL(URL);
+    }
+}
